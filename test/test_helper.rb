2578ca7d733b86c170afcd84f0b1fdad69cc9326
Florian Aßmann <florian.assmann@email.de>
2009-01-28 12:06:47 +0100

    Stubbed Rails internals, fixes specs.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,12 @@ require 'expectations'
 require 'action_view'
 
 module Rails
+  class Initializer
+    @commands = {}
+    def self.run(command, config = false)
+      @commands[command] = config
+    end
+  end
   @root = "#{ File.dirname __FILE__ }"
   class << self; attr_reader :root; end
   
@@ -14,4 +20,9 @@ module Rails
   class << self; attr_reader :configuration; end
 end
 
+class Controller
+  def self.master_helper_module() Module.new end
+end
+
 require "#{ File.dirname __FILE__ }/../init.rb"
+Shadows.controller = Controller.new
